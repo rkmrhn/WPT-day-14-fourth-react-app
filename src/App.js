9c7108904c31ba9c8a0245c305d8f1b5ea990003
Rohan Maurya <rohanmaurya1695@gmail.com>
2023-06-15 16:01:29 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <MessageDemo name="rohan"  />
+      <MessageDemo name="pankaj" />
+      <MessageDemo name="shubham" />
+    </>
   );
+}
+function MessageDemo(props){
+//props will behave like an object
+  return(
+    <>
+    <h1>hello {props.name}</h1>
+    </>
+  )
 }
 
 export default App;
