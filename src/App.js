a2047efb458e85a297622aeb4e53d666ca925f3a
Rohan Maurya <rohanmaurya1695@gmail.com>
2023-06-16 00:44:13 +0530

    comit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
 function App() {
   return (
     <>
-      <h1>Loop list</h1>
-      <TestLoop />
+      <h1>demo</h1>
+      <ListDemo />
     </>
   );
 }
-function TestLoop() {
-  //stateless
+// objective is to print a sentence n number of times
+function ListDemo() {
+  let data = "hello world";
   let list = ["delhi", "mumbai", "kolkata", "lucknow"];
-  // stateful:
-  // let [list, setList] = ["delhi", "mumbai", "kolkata", "lucknow"];
-
+  let list1 = [];
+  for (let i = 0; i < 100; i++) {
+    list1.push("");
+  }
   return (
     <>
-      <h1>data</h1>
-      {/**for each do not give output its a consumer function*/}
-      {list.forEach((item) => item)}
-      {/**it will give us input and output both */}
-      {list.map((item) => item)}
+      <h1>{data}</h1>
+      {/**print hello world 4 times */}
+      <h1>
+        {list.map((item) => (
+          <div>{data}</div>
+        ))}
+      </h1>
+
+      {/**print hello universe 100 times */}
+      <h1>
+        {" "}
+        {list1.map((item) => (
+          <div>hello universe</div>
+        ))}
+      </h1>
     </>
   );
 }
