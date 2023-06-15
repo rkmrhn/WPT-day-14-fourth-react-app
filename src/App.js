ac7cbc065adc37c3d2b1618f97f340c4d1e86eff
Rohan Maurya <rohanmaurya1695@gmail.com>
2023-06-15 16:05:54 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -4,17 +4,16 @@ import "./App.css";
 function App() {
   return (
     <>
-      <MessageDemo name="rohan"  />
-      <MessageDemo name="pankaj" />
-      <MessageDemo name="shubham" />
+      <MessageDemo name="rohan" email="rohan@gmail.com"  />
+      <MessageDemo name="pankaj" email="pankaj@gmail.com" />
+      <MessageDemo name="shubham" email="shubham@gmail.com" />
     </>
   );
 }
-function MessageDemo(props){
-//props will behave like an object
+function MessageDemo({name,email}){
   return(
     <>
-    <h1>hello {props.name}</h1>
+    <h1>hello {name} {email}</h1>
     </>
   )
 }
